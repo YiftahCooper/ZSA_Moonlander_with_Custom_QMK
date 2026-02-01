d5362874e3d97209142c347b086b556e5d8e1ca8
Yiftah <136400291+YiftahCooper@users.noreply.github.com>
2026-02-01 05:58:30 +0200

    manual Update rules.mk

--- 3aMQz/rules.mk
+++ 3aMQz/rules.mk
@@ -1,9 +1,13 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 LTO_ENABLE = yes
-CONSOLE_ENABLE = no
-COMMAND_ENABLE = no
 WEBUSB_ENABLE = yes
 ORYX_ENABLE = yes
 RGBLIGHT_ENABLE = no
 TAP_DANCE_ENABLE = yes
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+AUTO_SHIFT_ENABLE = yes
+MOUSEKEY_ENABLE = no
+RGB_MATRIX_CUSTOM_KB = yes
+SPACE_CADET_ENABLE = no
